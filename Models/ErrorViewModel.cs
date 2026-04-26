ad739682d7b1d8a602c3b40c4620b85b6a9c6f48
Andy-2214 <markxx32@gmail.com>
2026-04-25 22:13:31 -0500

    Version inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PaginaAdminSITP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
