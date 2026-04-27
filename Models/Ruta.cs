ef3c63c919784dd3ac294b6720b7d9e92d852a0a
Andy-2214 <markxx32@gmail.com>
2026-04-27 09:43:59 -0500

    Administrador

--- Models/Ruta.cs
+++ Models/Ruta.cs
@@ -1,4 +1,5 @@
 using Google.Cloud.Firestore;
+using System.Text.Json;
 
 namespace TuProyecto.Models
 {
@@ -19,5 +20,19 @@ namespace TuProyecto.Models
 
         [FirestoreProperty("avenidas")]
         public string Avenidas { get; set; }
+
+        [FirestoreProperty("coordenadas")]
+        public List<Dictionary<string, double>> Coordenadas { get; set; } = new();
+
+        // Para manejar el JSON del formulario
+        public string CoordenadasJson
+        {
+            get => JsonSerializer.Serialize(Coordenadas);
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    Coordenadas = JsonSerializer.Deserialize<List<Dictionary<string, double>>>(value) ?? new();
+            }
+        }
     }
 }
